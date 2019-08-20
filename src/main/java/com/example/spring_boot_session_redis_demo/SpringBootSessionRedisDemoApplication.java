ca4ca6210b7d2acc9a734c1244470dc7e5b1ebdd
chen_anguo <cag2050@163.com>
2019-08-20 20:00:28 +0800

    project init

---
+++ src/main/java/com/example/spring_boot_session_redis_demo/SpringBootSessionRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_session_redis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSessionRedisDemoApplication.class, args);
+    }
+
+}
